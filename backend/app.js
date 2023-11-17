896f0bf215394f157a97a56b5a02c8deb9dbb219
Effix <107648239+Effix1@users.noreply.github.com>
2023-11-17 15:11:50 +0100

    mise en place de schema de basse de données

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+const Objet = require('./models/Objet')
+
+mongoose.connect('mongodb+srv://Effix1st:Eloise2015@cluster0.oljcbaq.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -18,10 +26,13 @@ app.use((req, res, next) => {
 });
 //const mongoose = require ('mongoose');
 app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet crée !',
+  delete req.body._id;
+  const objet = new Objet({
+    ...req.body
   });
+  objet.save()
+  .then(()=> res.status(201).json({message : 'Objet en registrée'}))
+  .catch(error => res.status(400).json({error}))
 });
 
 app.get('/api/stuff', (req, res, next) => {
